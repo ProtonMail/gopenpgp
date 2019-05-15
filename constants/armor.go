3e4e36b76659f72599867ade9a883ae5a3bd4e90
Daniel Huigens <d.huigens@protonmail.com>
2019-05-15 11:37:57 +0200

    Update armor header URL

--- constants/armor.go
+++ constants/armor.go
@@ -3,7 +3,7 @@ package constants
 // Definitions for armored data
 const (
 	ArmorHeaderVersion = "GopenPGP 0.0.1 (" + Version + ")"
-	ArmorHeaderComment = "https://github.com/ProtonMail/gopenpgp"
+	ArmorHeaderComment = "https://gopenpgp.org"
 	PGPMessageHeader   = "PGP MESSAGE"
 	PublicKeyHeader    = "PGP PUBLIC KEY BLOCK"
 	PrivateKeyHeader   = "PGP PRIVATE KEY BLOCK"
