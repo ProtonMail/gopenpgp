d0539d481f4cfa64c7c1b3d65329eea358a9547c
zugzwang <fvial@protonmail.com>
2020-04-24 14:31:23 +0200

    Reduce complexity of SignatureCollector.Accept

--- crypto/signature_collector.go
+++ crypto/signature_collector.go
@@ -38,82 +38,76 @@ func (sc *SignatureCollector) Accept(
 	hasPlainSibling, isFirst, isLast bool,
 ) (err error) {
 	parentMediaType, params, _ := mime.ParseMediaType(header.Get("Content-Type"))
-	if parentMediaType == "multipart/signed" {
-		newPart, rawBody := gomime.GetRawMimePart(part, "--"+params["boundary"])
-		var multiparts []io.Reader
-		var multipartHeaders []textproto.MIMEHeader
-		if multiparts, multipartHeaders, err = gomime.GetMultipartParts(newPart, params); err == nil {
-			hasPlainChild := false
-			for _, header := range multipartHeaders {
-				mediaType, _, _ := mime.ParseMediaType(header.Get("Content-Type"))
-				if mediaType == "text/plain" {
-					hasPlainChild = true
-				}
-			}
-			if len(multiparts) != 2 {
-				sc.verified = newSignatureNotSigned()
-				// Invalid multipart/signed format just pass along
-				_, err = ioutil.ReadAll(rawBody)
-				if err != nil {
-					return err
-				}
 
-				for i, p := range multiparts {
-					if err = sc.target.Accept(p, multipartHeaders[i], hasPlainChild, true, true); err != nil {
-						return
-					}
-				}
+	if parentMediaType != "multipart/signed" {
+		return sc.target.Accept(part, header, hasPlainSibling, isFirst, isLast)
+	}
+
+	newPart, rawBody := gomime.GetRawMimePart(part, "--"+params["boundary"])
+	multiparts, multipartHeaders, err := gomime.GetMultipartParts(newPart, params)
+	if err != nil {
+		return
+	}
+
+	hasPlainChild := false
+	for _, header := range multipartHeaders {
+		mediaType, _, _ := mime.ParseMediaType(header.Get("Content-Type"))
+		hasPlainChild = (mediaType == "text/plain")
+	}
+	if len(multiparts) != 2 {
+		sc.verified = newSignatureNotSigned()
+		// Invalid multipart/signed format just pass along
+		if _, err = ioutil.ReadAll(rawBody); err != nil {
+			return err
+		}
+
+		for i, p := range multiparts {
+			if err = sc.target.Accept(p, multipartHeaders[i], hasPlainChild, true, true); err != nil {
 				return
 			}
-
-			// actual multipart/signed format
-			err = sc.target.Accept(multiparts[0], multipartHeaders[0], hasPlainChild, true, true)
-			if err != nil {
-				return err
-			}
-
-			partData, err := ioutil.ReadAll(multiparts[1])
-			if err != nil {
-				return err
-			}
-
-			decodedPart := gomime.DecodeContentEncoding(
-				bytes.NewReader(partData),
-				multipartHeaders[1].Get("Content-Transfer-Encoding"))
-
-			buffer, err := ioutil.ReadAll(decodedPart)
-			if err != nil {
-				return err
-			}
-			mediaType, _, _ := mime.ParseMediaType(header.Get("Content-Type"))
-			buffer, err = gomime.DecodeCharset(buffer, mediaType, params)
-			if err != nil {
-				return err
-			}
-			sc.signature = string(buffer)
-			str, _ := ioutil.ReadAll(rawBody)
-			rawBody = bytes.NewReader(str)
-			if sc.keyring != nil {
-				_, err = openpgp.CheckArmoredDetachedSignature(sc.keyring, rawBody, bytes.NewReader(buffer), sc.config)
-
-				if err != nil {
-					sc.verified = newSignatureFailed()
-				} else {
-					sc.verified = nil
-				}
-			} else {
-				sc.verified = newSignatureNoVerifier()
-			}
-			return nil
 		}
 		return
 	}
-	err = sc.target.Accept(part, header, hasPlainSibling, isFirst, isLast)
+
+	// actual multipart/signed format
+	err = sc.target.Accept(multiparts[0], multipartHeaders[0], hasPlainChild, true, true)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	partData, err := ioutil.ReadAll(multiparts[1])
+	if err != nil {
+		return err
+	}
+
+	decodedPart := gomime.DecodeContentEncoding(
+		bytes.NewReader(partData),
+		multipartHeaders[1].Get("Content-Transfer-Encoding"))
+
+	buffer, err := ioutil.ReadAll(decodedPart)
+	if err != nil {
+		return err
+	}
+	mediaType, _, _ := mime.ParseMediaType(header.Get("Content-Type"))
+	buffer, err = gomime.DecodeCharset(buffer, mediaType, params)
+	if err != nil {
+		return err
+	}
+	sc.signature = string(buffer)
+	str, _ := ioutil.ReadAll(rawBody)
+	rawBody = bytes.NewReader(str)
+	if sc.keyring != nil {
+		_, err = openpgp.CheckArmoredDetachedSignature(sc.keyring, rawBody, bytes.NewReader(buffer), sc.config)
+
+		if err != nil {
+			sc.verified = newSignatureFailed()
+		} else {
+			sc.verified = nil
+		}
+	} else {
+		sc.verified = newSignatureNoVerifier()
+	}
+	return err
 }
 
 // GetSignature collected by Accept
