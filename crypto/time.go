497d74786c070a71b0ca107ab7ddc493c1925aeb
Dimitar Solev <dimitar.solev@protonmail.com>
2020-07-21 13:07:48 +0200

    Improve logic for UpdateTime and formatting.

--- crypto/time.go
+++ crypto/time.go
@@ -7,10 +7,10 @@ import (
 
 // UpdateTime updates cached time.
 func UpdateTime(newTime int64) {
-	if(newTime > pgp.latestServerTime) {
+	if newTime > pgp.latestServerTime {
 		pgp.latestServerTime = newTime
+		pgp.latestClientTime = time.Now()
 	}
-	pgp.latestClientTime = time.Now()
 }
 
 // GetUnixTime gets latest cached time.
