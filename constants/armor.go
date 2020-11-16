5b1a42c2cdd40ec4701f41d112fbef78a20683dd
wussler <aron@wussler.it>
2020-11-16 09:51:51 +0100

    Release v2.1.1 (#100)

--- constants/armor.go
+++ constants/armor.go
@@ -3,7 +3,7 @@ package constants
 
 // Constants for armored data.
 const (
-	ArmorHeaderVersion = "GopenPGP 2.1.0"
+	ArmorHeaderVersion = "GopenPGP 2.1.1"
 	ArmorHeaderComment = "https://gopenpgp.org"
 	PGPMessageHeader   = "PGP MESSAGE"
 	PGPSignatureHeader = "PGP SIGNATURE"
