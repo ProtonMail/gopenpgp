8712df564997e2b7a9fe6c7f803ec1bfdfcb2cff
marin thiercelin <marin.thiercelin@protonmail.com>
2021-04-28 12:15:27 +0200

    workaround for reader problem with copies

--- crypto/keyring_streaming.go
+++ crypto/keyring_streaming.go
@@ -1,6 +1,7 @@
 package crypto
 
 import (
+	"fmt"
 	"io"
 	"time"
 
@@ -9,14 +10,22 @@ import (
 	"github.com/pkg/errors"
 )
 
-var EOF = io.EOF
-
 func IsEOF(err error) bool {
-	return errors.Is(err, EOF)
+	return errors.Is(err, io.EOF)
 }
 
-type Reader interface {
-	Read([]byte) (int, error)
+type GoMobileReadResult struct {
+	N     int
+	IsEOF bool
+	Data  []byte
+}
+
+func NewGoMobileReadResult(n int, eof bool, data []byte) *GoMobileReadResult {
+	return &GoMobileReadResult{n, eof, data}
+}
+
+type GoMobileReader interface {
+	Read(int) (*GoMobileReadResult, error)
 }
 
 type Writer interface {
@@ -64,14 +73,62 @@ func (keyRing *KeyRing) EncryptStream(
 }
 
 type PlainMessageReader struct {
-	Data     Reader
+	Data     GoMobileReader
 	TextType bool
 	Filename string
 	Time     uint32
 }
 
+type BridgingNative2GoReader struct {
+	reader GoMobileReader
+}
+
+func (d *BridgingNative2GoReader) Read(b []byte) (int, error) {
+	result, err := d.reader.Read(len(b))
+	if err != nil {
+		fmt.Printf("error while reading %v\n", err)
+		return 0, err
+	}
+	n := result.N
+	fmt.Printf("Read %d\n", n)
+	if n > 0 {
+		copy(b, result.Data[:n])
+		fmt.Printf("Bytes %x\n", b[:n])
+	}
+	if result.IsEOF {
+		fmt.Println("EOF")
+		err = io.EOF
+	}
+	return n, err
+}
+
+type BridgingGo2NativeReader struct {
+	reader io.Reader
+}
+
+func (d *BridgingGo2NativeReader) Read(max int) (*GoMobileReadResult, error) {
+	b := make([]byte, max)
+	n, err := d.reader.Read(b)
+	result := &GoMobileReadResult{}
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			fmt.Println("EOF")
+			result.IsEOF = true
+		} else {
+			return nil, err
+		}
+	}
+	result.N = n
+	fmt.Printf("Read %d\n", n)
+	if n > 0 {
+		result.Data = b[:n]
+		fmt.Printf("Bytes %x\n", b[:n])
+	}
+	return result, nil
+}
+
 func (keyRing *KeyRing) DecryptStream(
-	message Reader, verifyKey *KeyRing, verifyTime int64,
+	message GoMobileReader, verifyKey *KeyRing, verifyTime int64,
 ) (plainMessage *PlainMessageReader, err error) {
 	privKeyEntries := keyRing.entities
 	var additionalEntries openpgp.EntityList
@@ -86,7 +143,7 @@ func (keyRing *KeyRing) DecryptStream(
 
 	config := &packet.Config{Time: getTimeGenerator()}
 
-	messageDetails, err := openpgp.ReadMessage(message, privKeyEntries, nil, config)
+	messageDetails, err := openpgp.ReadMessage(&BridgingNative2GoReader{message}, privKeyEntries, nil, config)
 	if err != nil {
 		return nil, errors.Wrap(err, "gopenpgp: error in reading message")
 	}
@@ -97,7 +154,7 @@ func (keyRing *KeyRing) DecryptStream(
 	}
 
 	return &PlainMessageReader{
-		Data:     messageDetails.UnverifiedBody,
+		Data:     &BridgingGo2NativeReader{messageDetails.UnverifiedBody},
 		TextType: !messageDetails.LiteralData.IsBinary,
 		Filename: messageDetails.LiteralData.FileName,
 		Time:     messageDetails.LiteralData.Time,
