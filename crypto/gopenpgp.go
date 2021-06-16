7380f7391fd8707bf05163b12f263f8a8be11430
wussler <aron@wussler.it>
2021-06-16 17:24:25 +0200

    Refactor internals of client time (#134)

--- crypto/gopenpgp.go
+++ crypto/gopenpgp.go
@@ -1,13 +1,10 @@
 // Package crypto provides a high-level API for common OpenPGP functionality.
 package crypto
 
-import "time"
-
 // GopenPGP is used as a "namespace" for many of the functions in this package.
 // It is a struct that keeps track of time skew between server and client.
 type GopenPGP struct {
 	latestServerTime int64
-	latestClientTime time.Time
 	generationOffset int64
 }
 
