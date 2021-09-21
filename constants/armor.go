a00c613a034cd99a20190a0108ca0600ea3bf2c1
Aron Wussler <aron@wussler.it>
2021-09-21 13:15:16 +0200

    Bump version numbers

--- constants/armor.go
+++ constants/armor.go
@@ -3,7 +3,7 @@ package constants
 
 // Constants for armored data.
 const (
-	ArmorHeaderVersion = "GopenPGP 2.2.2"
+	ArmorHeaderVersion = "GopenPGP 2.2.3"
 	ArmorHeaderComment = "https://gopenpgp.org"
 	PGPMessageHeader   = "PGP MESSAGE"
 	PGPSignatureHeader = "PGP SIGNATURE"
