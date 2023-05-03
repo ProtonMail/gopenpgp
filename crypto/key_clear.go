b191ccdbb593ec4b8113a23e1ea1f3aecf69bdff
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-05-03 17:02:03 +0200

    Add new key type options to key generation

--- crypto/key_clear.go
+++ crypto/key_clear.go
@@ -146,7 +146,7 @@ func clearX25519PrivateKey(priv *x25519.PrivateKey) error {
 }
 
 func clearEd25519PrivateKey(priv *ed25519.PrivateKey) error {
-	clearMem(priv.Key[:ed25519.SeedSize])
+	clearMem(priv.Key[:ed25519.PointSize])
 
 	return nil
 }
@@ -158,7 +158,7 @@ func clearX448PrivateKey(priv *x448.PrivateKey) error {
 }
 
 func clearEd448PrivateKey(priv *ed448.PrivateKey) error {
-	clearMem(priv.Key[:ed448.SeedSize])
+	clearMem(priv.Key[:ed448.PointSize])
 
 	return nil
 }
