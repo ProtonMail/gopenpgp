a312e76f667e1a2a31114e9b32ae884e3826a580
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-05-09 13:53:16 +0200

    Add bytes armor functions

--- internal/armor.go
+++ internal/armor.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"bytes"
 	"strings"
 
 	"github.com/ProtonMail/go-crypto/openpgp/armor"
@@ -10,6 +11,16 @@ import (
 // Unarmor unarmors an armored string.
 func Unarmor(input string) (*armor.Block, error) {
 	io := strings.NewReader(input)
+	b, err := armor.Decode(io)
+	if err != nil {
+		return nil, errors.Wrap(err, "gopenpgp: unable to unarmor")
+	}
+	return b, nil
+}
+
+// Unarmor unarmors an armored byte string.
+func UnarmorBytes(input []byte) (*armor.Block, error) {
+	io := bytes.NewReader(input)
 	b, err := armor.Decode(io)
 	if err != nil {
 		return nil, errors.Wrap(err, "gopenpgp: unable to unarmor")
