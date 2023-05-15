fcb45ca2095f9d6f8ff0af048c854689c5e43b4d
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-05-15 15:40:37 +0200

    Move mobile specific logic to mobile package

---
+++ mobile/mobile.go
@@ -0,0 +1,10 @@
+package mobile
+
+import "runtime/debug"
+
+// FreeOSMemory can be used to explicitly
+// call the garbage collector and
+// return the unused memory to the OS.
+func FreeOSMemory() {
+	debug.FreeOSMemory()
+}
