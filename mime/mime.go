7ca94591f6644e6d56ec0c94070e8b0ecf714654
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-05-15 09:59:54 +0200

    Add decryption function for mime messages

---
+++ mime/mime.go
@@ -0,0 +1,163 @@
+package mime
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/mail"
+	"net/textproto"
+
+	gomime "github.com/ProtonMail/go-mime"
+	"github.com/ProtonMail/gopenpgp/v2/constants"
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
+	"github.com/ProtonMail/gopenpgp/v2/internal"
+	"github.com/pkg/errors"
+)
+
+// MIMECallbacks defines callback methods to process a MIME message.
+type MIMECallbacks interface {
+	OnBody(body string, mimetype string)
+	OnAttachment(headers string, data []byte)
+	// Encrypted headers can be in an attachment and thus be placed at the end of the mime structure.
+	OnEncryptedHeaders(headers string)
+	OnVerified(verified int)
+	OnError(err error)
+}
+
+type decryption struct {
+	DecryptionHandle crypto.PGPDecryption
+	VerifyHandle     crypto.PGPVerify
+}
+
+func NewDecryptionFromHandles(
+	decryptionHandle crypto.PGPDecryption,
+	verifyHandle crypto.PGPVerify,
+) (*decryption, error) {
+	if decryptionHandle == nil {
+		return nil, errors.New("gopenpgp: no decryption handle provided")
+	}
+	return &decryption{
+		DecryptionHandle: decryptionHandle,
+		VerifyHandle:     verifyHandle,
+	}, nil
+}
+
+func NewDecryption(
+	pgp *crypto.PGPHandle,
+	decryptionKeys *crypto.KeyRing,
+	verifyKey *crypto.KeyRing,
+	armored bool,
+	verifyTime int64,
+) (*decryption, error) {
+	decryptHandle, err := pgp.Decryption().
+		DecryptionKeys(decryptionKeys).
+		VerifyKeys(verifyKey).
+		WithArmor(armored).
+		VerifyTime(verifyTime).New()
+	if err != nil {
+		return nil, err
+	}
+	var verifyHandle crypto.PGPVerify
+	if verifyKey != nil {
+		verifyHandle, err = pgp.Verify().
+			VerifyKeys(verifyKey).
+			VerifyTime(verifyTime).
+			Armored().New()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &decryption{
+		DecryptionHandle: decryptHandle,
+		VerifyHandle:     verifyHandle,
+	}, nil
+}
+
+// DecryptMIMEMessage decrypts a MIME message.
+func (d *decryption) DecryptMIMEMessage(
+	message []byte,
+	callbacks MIMECallbacks,
+) {
+	decResult, err := d.DecryptionHandle.Decrypt(message)
+	if err != nil {
+		callbacks.OnError(err)
+		return
+	}
+	decryptedMessage := decResult.Result()
+	embeddedSigError, _ := separateSigError(decResult.SignatureError())
+
+	body, attachments, attachmentHeaders, err := d.parseMIME(decryptedMessage)
+	mimeSigError, err := separateSigError(err)
+	if err != nil {
+		callbacks.OnError(err)
+		return
+	}
+	// We only consider the signature to be failed if both embedded and mime verification failed
+	if decResult.HasSignatureError() && mimeSigError != nil {
+		callbacks.OnError(embeddedSigError)
+		callbacks.OnError(mimeSigError)
+		callbacks.OnVerified(prioritizeSignatureErrors(embeddedSigError, mimeSigError))
+	} else if d.VerifyHandle != nil {
+		callbacks.OnVerified(constants.SIGNATURE_OK)
+	}
+	bodyContent, bodyMimeType := body.GetBody()
+	bodyContentSanitized := internal.SanitizeString(bodyContent)
+	callbacks.OnBody(bodyContentSanitized, bodyMimeType)
+	for i := 0; i < len(attachments); i++ {
+		callbacks.OnAttachment(attachmentHeaders[i], []byte(attachments[i]))
+	}
+	callbacks.OnEncryptedHeaders("")
+}
+
+// ----- INTERNAL FUNCTIONS -----
+
+func prioritizeSignatureErrors(signatureErrs ...*crypto.SignatureVerificationError) (maxError int) {
+	// select error with the highest value, if any
+	// FAILED > NO VERIFIER > NOT SIGNED > SIGNATURE OK
+	maxError = constants.SIGNATURE_OK
+	for _, err := range signatureErrs {
+		if err.Status > maxError {
+			maxError = err.Status
+		}
+	}
+	return
+}
+
+func separateSigError(err error) (*crypto.SignatureVerificationError, error) {
+	sigErr := &crypto.SignatureVerificationError{}
+	if errors.As(err, sigErr) {
+		return sigErr, nil
+	}
+	return nil, err
+}
+
+func (d *decryption) parseMIME(
+	mimeBody []byte,
+) (*gomime.BodyCollector, []string, []string, error) {
+	mm, err := mail.ReadMessage(bytes.NewReader(mimeBody))
+	if err != nil {
+		return nil, nil, nil, errors.Wrap(err, "gopenpgp: error in reading message")
+	}
+
+	h := textproto.MIMEHeader(mm.Header)
+	mmBodyData, err := ioutil.ReadAll(mm.Body)
+	if err != nil {
+		return nil, nil, nil, errors.Wrap(err, "gopenpgp: error in reading message body data")
+	}
+
+	printAccepter := gomime.NewMIMEPrinter()
+	bodyCollector := gomime.NewBodyCollector(printAccepter)
+	attachmentsCollector := gomime.NewAttachmentsCollector(bodyCollector)
+	mimeVisitor := gomime.NewMimeVisitor(attachmentsCollector)
+
+	signatureCollector := newSignatureCollector(mimeVisitor, d.VerifyHandle)
+
+	err = gomime.VisitAll(bytes.NewReader(mmBodyData), h, signatureCollector)
+	if err == nil && d.VerifyHandle != nil {
+		err = signatureCollector.verified
+	}
+
+	return bodyCollector,
+		attachmentsCollector.GetAttachments(),
+		attachmentsCollector.GetAttHeaders(),
+		err
+}
---
+++ mime/mime_test.go
@@ -0,0 +1,339 @@
+package mime
+
+import (
+	"errors"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
+	"github.com/stretchr/testify/assert"
+)
+
+// Corresponding key in testdata/mime_privateKey.
+var MIMEKeyPassword = []byte("test")
+
+type Callbacks struct {
+	Testing *testing.T
+}
+
+func (t *Callbacks) OnBody(body string, mimetype string) {
+	assert.Exactly(t.Testing, readTestFile("mime_decryptedBody", false), body)
+}
+
+func (t Callbacks) OnAttachment(headers string, data []byte) {
+	assert.Exactly(t.Testing, 1, data)
+}
+
+func (t Callbacks) OnEncryptedHeaders(headers string) {
+	assert.Exactly(t.Testing, "", headers)
+}
+
+func (t Callbacks) OnVerified(verified int) {
+}
+
+func (t Callbacks) OnError(err error) {
+	t.Testing.Fatal("Error in decrypting MIME message: ", err)
+}
+
+func TestDecrypt(t *testing.T) {
+	callbacks := Callbacks{
+		Testing: t,
+	}
+
+	privateKey, err := crypto.NewKeyFromArmored(readTestFile("mime_privateKey", false))
+	if err != nil {
+		t.Fatal("Cannot unarmor private key:", err)
+	}
+
+	privateKey, err = privateKey.Unlock(MIMEKeyPassword)
+	if err != nil {
+		t.Fatal("Cannot unlock private key:", err)
+	}
+
+	privateKeyRing, err := crypto.NewKeyRing(privateKey)
+	if err != nil {
+		t.Fatal("Cannot create private keyring:", err)
+	}
+
+	message := readTestFileBytes("mime_pgpMessage")
+	pgp := crypto.PGP()
+	mimeHandle, err := NewDecryption(pgp, privateKeyRing, nil, true, 1557754627)
+	if err != nil {
+		t.Fatal("Cannot create mime handle:", err)
+	}
+
+	mimeHandle.DecryptMIMEMessage(message, &callbacks)
+}
+
+type testMIMECallbacks struct {
+	onBody       []struct{ body, mimetype string }
+	onAttachment []struct {
+		headers string
+		data    []byte
+	}
+	onEncryptedHeaders []string
+	onVerified         []int
+	onError            []error
+}
+
+func (tc *testMIMECallbacks) OnBody(body string, mimetype string) {
+	tc.onBody = append(tc.onBody, struct {
+		body     string
+		mimetype string
+	}{body, mimetype})
+}
+
+func (tc *testMIMECallbacks) OnAttachment(headers string, data []byte) {
+	tc.onAttachment = append(tc.onAttachment, struct {
+		headers string
+		data    []byte
+	}{headers, data})
+}
+
+func (tc *testMIMECallbacks) OnEncryptedHeaders(headers string) {
+	tc.onEncryptedHeaders = append(tc.onEncryptedHeaders, headers)
+}
+
+func (tc *testMIMECallbacks) OnVerified(status int) {
+	tc.onVerified = append(tc.onVerified, status)
+}
+
+func (tc *testMIMECallbacks) OnError(err error) {
+	tc.onError = append(tc.onError, err)
+}
+
+func loadPrivateKeyRing(file string, passphrase string) (*crypto.KeyRing, error) {
+	armored, err := ioutil.ReadFile(filepath.Clean(file))
+	if err != nil {
+		return nil, err
+	}
+	unlockedKey, err := crypto.NewPrivateKeyFromArmored(string(armored), []byte(passphrase))
+	if err != nil {
+		return nil, err
+	}
+	keyRing, err := crypto.NewKeyRing(unlockedKey)
+	if err != nil {
+		return nil, err
+	}
+	return keyRing, nil
+}
+
+func loadPublicKeyRing(file string) (*crypto.KeyRing, error) {
+	armored, err := ioutil.ReadFile(filepath.Clean(file))
+	if err != nil {
+		return nil, err
+	}
+	key, err := crypto.NewKeyFromArmored(string(armored))
+	if err != nil {
+		return nil, err
+	}
+	if key.IsPrivate() {
+		publicKey, err := key.GetPublicKey()
+		if err != nil {
+			return nil, err
+		}
+		key, err = crypto.NewKey(publicKey)
+		if err != nil {
+			return nil, err
+		}
+	}
+	keyRing, err := crypto.NewKeyRing(key)
+	if err != nil {
+		return nil, err
+	}
+	return keyRing, nil
+}
+
+func loadMessage(file string) ([]byte, error) {
+	armored, err := ioutil.ReadFile(filepath.Clean(file))
+	if err != nil {
+		return nil, err
+	}
+	return armored, nil
+}
+
+func runScenario(t *testing.T, messageFile string) *testMIMECallbacks {
+	decryptionKeyRing, err := loadPrivateKeyRing("testdata/mime/decryption-key.asc", "test_passphrase")
+	if err != nil {
+		t.Errorf("Failed to load decryption key %v", err)
+	}
+	verificationKeyRing, err := loadPublicKeyRing("testdata/mime/verification-key.asc")
+	if err != nil {
+		t.Errorf("Failed to load verification key %v", err)
+	}
+	message, err := loadMessage(messageFile)
+	if err != nil {
+		t.Errorf("Failed to load message %v", err)
+	}
+	callbacks := &testMIMECallbacks{}
+	pgp := crypto.PGP()
+	mimeHandle, err := NewDecryption(pgp, decryptionKeyRing, verificationKeyRing, true, 1557754627)
+	if err != nil {
+		t.Errorf("Failed to load message %v", err)
+	}
+	mimeHandle.DecryptMIMEMessage(message, callbacks)
+	return callbacks
+}
+
+func compareStatus(expected []int, actual []int, t *testing.T) {
+	if len(actual) != len(expected) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	} else {
+		for i, actualStatus := range actual {
+			if actualStatus != expected[i] {
+				t.Errorf("Expected status %v, got %v", expected[i], actualStatus)
+			}
+		}
+	}
+}
+
+func TestMessageVerificationOkOk(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_00.asc")
+	if len(callbackResults.onError) != 0 {
+		for _, err := range callbackResults.onError {
+			t.Errorf("Expected no errors got %v", err)
+		}
+	}
+	expectedStatus := []int{0}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationOkNotSigned(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_01.asc")
+	if len(callbackResults.onError) != 0 {
+		for _, err := range callbackResults.onError {
+			t.Errorf("Expected no errors got %v", err)
+		}
+	}
+	expectedStatus := []int{0}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationOkNoVerifier(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_02.asc")
+	if len(callbackResults.onError) != 0 {
+		for _, err := range callbackResults.onError {
+			t.Errorf("Expected no errors got %v", err)
+		}
+	}
+	expectedStatus := []int{0}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationOkFailed(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_03.asc")
+	if len(callbackResults.onError) != 0 {
+		for _, err := range callbackResults.onError {
+			t.Errorf("Expected no errors got %v", err)
+		}
+	}
+	expectedStatus := []int{0}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationNotSignedOk(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_10.asc")
+	if len(callbackResults.onError) != 0 {
+		for _, err := range callbackResults.onError {
+			t.Errorf("Expected no errors got %v", err)
+		}
+	}
+	expectedStatus := []int{0}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func checkIsSigErr(t *testing.T, err error) int {
+	sigErr := &crypto.SignatureVerificationError{}
+	if errors.As(err, &sigErr) {
+		return sigErr.Status
+	}
+	t.Errorf("Expected a signature verification error, got %v", err)
+	return -1
+}
+
+func compareErrors(expected []crypto.SignatureVerificationError, actual []error, t *testing.T) {
+	if len(actual) != len(expected) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	} else {
+		for i, err := range actual {
+			actualStatus := checkIsSigErr(t, err)
+			if actualStatus != expected[i].Status {
+				t.Errorf("Expected sig error with status %v, got %v", expected[i].Status, actualStatus)
+			}
+		}
+	}
+}
+
+func TestMessageVerificationNotSignedNotSigned(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_11.asc")
+	var expectedErrors = []crypto.SignatureVerificationError{newSignatureNotSigned(), newSignatureNotSigned()}
+	compareErrors(expectedErrors, callbackResults.onError, t)
+	expectedStatus := []int{1}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationNotSignedNoVerifier(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_12.asc")
+	var expectedErrors = []crypto.SignatureVerificationError{newSignatureNotSigned(), newSignatureNoVerifier()}
+	compareErrors(expectedErrors, callbackResults.onError, t)
+	expectedStatus := []int{2}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationNotSignedFailed(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_13.asc")
+	var expectedErrors = []crypto.SignatureVerificationError{newSignatureNotSigned(), newSignatureFailed(nil)}
+	compareErrors(expectedErrors, callbackResults.onError, t)
+	expectedStatus := []int{3}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationNoVerifierOk(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_20.asc")
+	var expectedErrors = []crypto.SignatureVerificationError{}
+	compareErrors(expectedErrors, callbackResults.onError, t)
+	expectedStatus := []int{0}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationNoVerifierNotSigned(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_21.asc")
+	var expectedErrors = []crypto.SignatureVerificationError{newSignatureNoVerifier(), newSignatureNotSigned()}
+	compareErrors(expectedErrors, callbackResults.onError, t)
+	expectedStatus := []int{2}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationNoVerifierNoVerifier(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_22.asc")
+	var expectedErrors = []crypto.SignatureVerificationError{newSignatureNoVerifier(), newSignatureNoVerifier()}
+	compareErrors(expectedErrors, callbackResults.onError, t)
+	expectedStatus := []int{2}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func TestMessageVerificationNoVerifierFailed(t *testing.T) {
+	callbackResults := runScenario(t, "testdata/mime/scenario_23.asc")
+	var expectedErrors = []crypto.SignatureVerificationError{newSignatureNoVerifier(), newSignatureFailed(nil)}
+	compareErrors(expectedErrors, callbackResults.onError, t)
+	expectedStatus := []int{3}
+	compareStatus(expectedStatus, callbackResults.onVerified, t)
+}
+
+func readTestFile(name string, trimNewlines bool) string {
+	data := string(readTestFileBytes(name))
+	if trimNewlines {
+		return strings.TrimRight(data, "\n")
+	}
+	return data
+}
+
+func readTestFileBytes(name string) []byte {
+	data, err := ioutil.ReadFile("testdata/" + name)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
