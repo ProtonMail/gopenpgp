b5e4083e157bb47d82e4bccd99a096d894002284
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-05-24 11:42:23 +0200

    Make the MIME decryption API more concise

--- mime/mime.go
+++ mime/mime.go
@@ -23,59 +23,17 @@ type MIMECallbacks interface {
 	OnError(err error)
 }
 
-type decryption struct {
-	DecryptionHandle crypto.PGPDecryption
-	VerifyHandle     crypto.PGPVerify
-}
-
-func NewDecryptionFromHandles(
+// DecryptMIMEMessage decrypts and verifies a MIME message.
+// The decryptionHandle is used to decrypt and verify the message, while
+// the verifyHandle is used to verify the signature contained in the decrypted mime message.
+// The verifyHandle can be nil.
+func DecryptMIMEMessage(
+	message []byte,
 	decryptionHandle crypto.PGPDecryption,
 	verifyHandle crypto.PGPVerify,
-) (*decryption, error) {
-	if decryptionHandle == nil {
-		return nil, errors.New("gopenpgp: no decryption handle provided")
-	}
-	return &decryption{
-		DecryptionHandle: decryptionHandle,
-		VerifyHandle:     verifyHandle,
-	}, nil
-}
-
-func NewDecryption(
-	pgp *crypto.PGPHandle,
-	decryptionKeys *crypto.KeyRing,
-	verifyKey *crypto.KeyRing,
-	verifyTime int64,
-) (*decryption, error) {
-	decryptHandle, err := pgp.Decryption().
-		DecryptionKeys(decryptionKeys).
-		VerifyKeys(verifyKey).
-		VerifyTime(verifyTime).New()
-	if err != nil {
-		return nil, err
-	}
-	var verifyHandle crypto.PGPVerify
-	if verifyKey != nil {
-		verifyHandle, err = pgp.Verify().
-			VerifyKeys(verifyKey).
-			VerifyTime(verifyTime).
-			New()
-		if err != nil {
-			return nil, err
-		}
-	}
-	return &decryption{
-		DecryptionHandle: decryptHandle,
-		VerifyHandle:     verifyHandle,
-	}, nil
-}
-
-// DecryptMIMEMessage decrypts a MIME message.
-func (d *decryption) DecryptMIMEMessage(
-	message []byte,
 	callbacks MIMECallbacks,
 ) {
-	decResult, err := d.DecryptionHandle.Decrypt(message)
+	decResult, err := decryptionHandle.Decrypt(message)
 	if err != nil {
 		callbacks.OnError(err)
 		return
@@ -83,7 +41,7 @@ func (d *decryption) DecryptMIMEMessage(
 	decryptedMessage := decResult.Result()
 	embeddedSigError, _ := separateSigError(decResult.SignatureError())
 
-	body, attachments, attachmentHeaders, err := d.parseMIME(decryptedMessage)
+	body, attachments, attachmentHeaders, err := parseMIME(decryptedMessage, verifyHandle)
 	mimeSigError, err := separateSigError(err)
 	if err != nil {
 		callbacks.OnError(err)
@@ -94,7 +52,7 @@ func (d *decryption) DecryptMIMEMessage(
 		callbacks.OnError(embeddedSigError)
 		callbacks.OnError(mimeSigError)
 		callbacks.OnVerified(prioritizeSignatureErrors(embeddedSigError, mimeSigError))
-	} else if d.VerifyHandle != nil {
+	} else if verifyHandle != nil {
 		callbacks.OnVerified(constants.SIGNATURE_OK)
 	}
 	bodyContent, bodyMimeType := body.GetBody()
@@ -128,8 +86,9 @@ func separateSigError(err error) (*crypto.SignatureVerificationError, error) {
 	return nil, err
 }
 
-func (d *decryption) parseMIME(
+func parseMIME(
 	mimeBody []byte,
+	verifyHandle crypto.PGPVerify,
 ) (*gomime.BodyCollector, []string, []string, error) {
 	mm, err := mail.ReadMessage(bytes.NewReader(mimeBody))
 	if err != nil {
@@ -147,10 +106,10 @@ func (d *decryption) parseMIME(
 	attachmentsCollector := gomime.NewAttachmentsCollector(bodyCollector)
 	mimeVisitor := gomime.NewMimeVisitor(attachmentsCollector)
 
-	signatureCollector := newSignatureCollector(mimeVisitor, d.VerifyHandle)
+	signatureCollector := newSignatureCollector(mimeVisitor, verifyHandle)
 
 	err = gomime.VisitAll(bytes.NewReader(mmBodyData), h, signatureCollector)
-	if err == nil && d.VerifyHandle != nil {
+	if err == nil && verifyHandle != nil {
 		err = signatureCollector.verified
 	}
 
