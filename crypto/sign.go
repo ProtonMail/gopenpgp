4c0338fe18a63ae008eea7580ae6300eb7ec1769
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-06-15 12:17:52 +0200

    Fix issue with createVerifyResult if multiple signatures are present

--- crypto/sign.go
+++ crypto/sign.go
@@ -13,8 +13,7 @@ type PGPSign interface {
 	// Once close is called on the returned WriteCloser the final signature is written to the output.
 	// Thus, the returned WriteCloser must be closed after the plaintext has been written.
 	SigningWriter(output Writer) (WriteCloser, error)
-	// SignDetached creates a detached or inline signature from the provided byte slice.
-	// Metadata is only considered for inline signatures that embed the data in a literal data packet.
+	// Sign creates a detached or inline signature from the provided byte slice.
 	Sign(message []byte) ([]byte, error)
 	// SignCleartext produces an armored cleartext message according to the specification.
 	// Returns an armored message even if the PGPSign is not configured for armored output.
