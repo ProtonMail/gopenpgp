3ee215d01a697b61313c74917b09823cb3e071c1
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-02-09 14:41:03 +0100

    feat(key): Allow to override algorithm in key generation

--- crypto/key_generation.go
+++ crypto/key_generation.go
@@ -4,6 +4,20 @@ import (
 	"github.com/ProtonMail/go-crypto/openpgp/packet"
 )
 
+// Integer enum for go-mobile compatibility.
+const (
+	// KeyGenerationRSA4096 allows to override the output key algorithm in key generation to rsa 4096.
+	KeyGenerationRSA4096 int = 1
+	// KeyGenerationC25519 allows to override the output key algorithm in key generation to curve25519.
+	KeyGenerationC25519 int = 2
+	// KeyGenerationC25519 allows to override the output key algorithm in key generation to curve25519 crypto refresh.
+	KeyGenerationC25519Refresh int = 3
+	// KeyGenerationC448 allows to override the output key algorithm in key generation to curve448.
+	KeyGenerationC448 int = 4
+	// KeyGenerationC448Refresh allows to override the output key algorithm in key generation to curve448 crypto refresh.
+	KeyGenerationC448Refresh int = 5
+)
+
 type KeyGenerationProfile interface {
 	KeyGenerationConfig(securityLevel int8) *packet.Config
 }
