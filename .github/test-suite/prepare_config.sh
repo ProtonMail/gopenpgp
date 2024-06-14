460f11155da1f78f2585de62e9a13ba342fc4862
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-06-14 10:50:05 +0200

    ci: Update inertop-test-suite to latest version

--- .github/test-suite/prepare_config.sh
+++ .github/test-suite/prepare_config.sh
@@ -7,6 +7,6 @@ cat $CONFIG_TEMPLATE \
     | sed "s@__GOSOP_MAIN__@${GOSOP_MAIN}@g" \
     | sed "s@__SQOP__@${SQOP}@g" \
     | sed "s@__GPGME_SOP__@${GPGME_SOP}@g" \
-    | sed "s@__SOP_OPENPGPJS__@${SOP_OPENPGPJS}@g" \
+    | sed "s@__SOP_OPENPGPJS__@${SOP_OPENPGPJS_V2}@g" \
     | sed "s@__RNP_SOP__@${RNP_SOP}@g" \
     > $CONFIG_OUTPUT
