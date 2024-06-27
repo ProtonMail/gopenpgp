38e83de1feb728296851ded022d8c5dc56b5c4e7
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-06-27 09:54:46 +0200

    ci: Update CI to be on par with v3

--- .github/test-suite/build_gosop.sh
+++ .github/test-suite/build_gosop.sh
@@ -1,4 +1,6 @@
+VERSION=$(awk '/^module github.com\/ProtonMail\/gopenpgp\/v[0-9]+/ {print $NF}' gopenpgp/go.mod | awk -F'v' '{print $2}')
+
 cd gosop
-echo "replace github.com/ProtonMail/gopenpgp/v2 => ../gopenpgp" >> go.mod
-go get github.com/ProtonMail/gopenpgp/v2/crypto
+echo "replace github.com/ProtonMail/gopenpgp/v${VERSION} => ../gopenpgp" >> go.mod
+go get github.com/ProtonMail/gopenpgp/v${VERSION}/crypto
 go build .
