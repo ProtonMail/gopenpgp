b15de20dc67c5c5aa8afa5f66aacc17374a30426
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-08-27 09:42:43 +0200

    ci: Update gosop branch for GopenPGP-v2

--- .github/test-suite/determine_gosop_branch.sh
+++ .github/test-suite/determine_gosop_branch.sh
@@ -3,5 +3,5 @@ VERSION=$(awk '/^module github.com\/ProtonMail\/gopenpgp\/v[0-9]+/ {print $NF}' 
 if [ "$VERSION" -eq 3 ]; then
   echo "gosop-gopenpgp-v3"
 else
-  echo "main"
+  echo "gosop-gopenpgp-v2"
 fi
