57fa8e76f80fee5201f9efad0cfda27583eefd50
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-09-12 11:50:35 +0200

    feat: Add PQC support and PQC & Symmetric profiles

--- profile/preset.go
+++ profile/preset.go
@@ -80,3 +80,65 @@ func RFC9580() *Custom {
 		V6: true,
 	}
 }
+
+func PQC() *Custom {
+	setKeyAlgorithm := func(cfg *packet.Config, securityLevel int8) {
+		cfg.Algorithm = packet.PubKeyAlgoMldsa65Ed25519
+	}
+	return &Custom{
+		SetKeyAlgorithm:      setKeyAlgorithm,
+		Hash:                 crypto.SHA512,
+		CipherEncryption:     packet.CipherAES256,
+		CipherKeyEncryption:  packet.CipherAES256,
+		CompressionAlgorithm: packet.CompressionZLIB,
+		AeadKeyEncryption: &packet.AEADConfig{
+			DefaultMode: packet.AEADModeGCM,
+		},
+		AeadEncryption: &packet.AEADConfig{
+			DefaultMode: packet.AEADModeGCM,
+		},
+		CompressionConfiguration: &packet.CompressionConfig{
+			Level: 6,
+		},
+		S2kKeyEncryption: &s2k.Config{
+			S2KMode:      s2k.Argon2S2K,
+			Argon2Config: &s2k.Argon2Config{},
+		},
+		S2kEncryption: &s2k.Config{
+			S2KMode:      s2k.Argon2S2K,
+			Argon2Config: &s2k.Argon2Config{},
+		},
+		V6: true,
+	}
+}
+
+func Symmetric() *Custom {
+	setKeyAlgorithm := func(cfg *packet.Config, securityLevel int8) {
+		cfg.Algorithm = packet.ExperimentalPubKeyAlgoHMAC
+	}
+	return &Custom{
+		SetKeyAlgorithm:      setKeyAlgorithm,
+		Hash:                 crypto.SHA512,
+		CipherEncryption:     packet.CipherAES256,
+		CipherKeyEncryption:  packet.CipherAES256,
+		CompressionAlgorithm: packet.CompressionZLIB,
+		AeadKeyEncryption: &packet.AEADConfig{
+			DefaultMode: packet.AEADModeGCM,
+		},
+		AeadEncryption: &packet.AEADConfig{
+			DefaultMode: packet.AEADModeGCM,
+		},
+		CompressionConfiguration: &packet.CompressionConfig{
+			Level: 6,
+		},
+		S2kKeyEncryption: &s2k.Config{
+			S2KMode:      s2k.Argon2S2K,
+			Argon2Config: &s2k.Argon2Config{},
+		},
+		S2kEncryption: &s2k.Config{
+			S2KMode:      s2k.Argon2S2K,
+			Argon2Config: &s2k.Argon2Config{},
+		},
+		V6: true,
+	}
+}
