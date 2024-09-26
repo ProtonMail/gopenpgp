1136c7d026aeb4e6005a0fdf78406db4246273f1
Lukas Burkhalter <10532077+lubux@users.noreply.github.com>
2024-09-26 12:03:48 +0200

    feat: Add SHA256 fingerprint getter (#299)
    * feat: Add SHA256 fingerprint getter to key

--- crypto/key_test.go
+++ crypto/key_test.go
@@ -374,6 +374,8 @@ func TestGetSHA256FingerprintsV4(t *testing.T) {
 	assert.Len(t, sha256Fingerprints, 2)
 	assert.Exactly(t, "d9ac0b857da6d2c8be985b251a9e3db31e7a1d2d832d1f07ebe838a9edce9c24", sha256Fingerprints[0])
 	assert.Exactly(t, "203dfba1f8442c17e59214d9cd11985bfc5cc8721bb4a71740dd5507e58a1a0d", sha256Fingerprints[1])
+
+	assert.Exactly(t, "d9ac0b857da6d2c8be985b251a9e3db31e7a1d2d832d1f07ebe838a9edce9c24", publicKey.GetSHA256Fingerprint())
 }
 
 func TestGetEntity(t *testing.T) {
