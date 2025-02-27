8dac03bb92f572dbf0e1011a58513d8c87818624
Lukas Burkhalter <10532077+lubux@users.noreply.github.com>
2025-02-27 13:46:28 +0100

    chore: Prepare release 2.8.3 (#328)

--- constants/armor.go
+++ constants/armor.go
@@ -3,7 +3,7 @@ package constants
 
 // Constants for armored data.
 const (
-	ArmorHeaderVersion = "GopenPGP 2.8.2"
+	ArmorHeaderVersion = "GopenPGP 2.8.3"
 	ArmorHeaderComment = "https://gopenpgp.org"
 	PGPMessageHeader   = "PGP MESSAGE"
 	PGPSignatureHeader = "PGP SIGNATURE"
